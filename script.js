27a87f40948c0144b85498f97e6192aecec065a3
PradhyumShivhare <143643006+pradxnebula@users.noreply.github.com>
2023-09-23 23:28:33 +0530

    added changes

--- script.js
+++ script.js
@@ -1,81 +1,93 @@
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Array of image and text combinations
-//     var genreArray = [
-//         { text: "Podcasts", image: "image1.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Ambient", image: "image4.jpeg" },
-//         { text: "Ambient", image: "image4.jpeg" },
-//         { text: "Ambient", image: "image4.jpeg" },
-//         { text: "Ambient", image: "image4.jpeg" },
-//         { text: "Ambient", image: "image4.jpeg" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Indie", image: "image3.png" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Music", image: "image2.jpeg" },
-//         { text: "Podcasts", image: "image1.jpeg" },
-//         { text: "Podcasts", image: "image1.jpeg" },
-//         { text: "Ambient", image: "image4.jpeg" },
-
-//         // Add more combinations here
-//     ];
-
-//     // Shuffle the genreArray to randomize the combinations
-//     shuffleArray(genreArray);
-
-//     // Get all elements with the class "genre-img" and "genre-text"
-//     var imgElements = document.querySelectorAll("#genre-img");
-//     var textElements = document.querySelectorAll("#genre-text");
-
-//     // Function to shuffle an array randomly
-//     function shuffleArray(array) {
-//         for (var i = array.length - 1; i > 0; i--) {
-//             var j = Math.floor(Math.random() * (i + 1));
-//             [array[i], array[j]] = [array[j], array[i]];
-//         }
-//     }
-
-//     // Loop through each card and assign the shuffled combinations
-//     for (var i = 0; i < imgElements.length; i++) {
-//         imgElements[i].src = "/images/" + genreArray[i].image;
-//         textElements[i].textContent = genreArray[i].text;
-//     }
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
     // Code for randomizing text and image combinations
-    var genreArray = [
-        { text: "Podcasts", image: "image1.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Ambient", image: "image4.jpeg" },
-        { text: "Ambient", image: "image4.jpeg" },
-        { text: "Ambient", image: "image4.jpeg" },
-        { text: "Ambient", image: "image4.jpeg" },
-        { text: "Ambient", image: "image4.jpeg" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Indie", image: "image3.png" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Music", image: "image2.jpeg" },
-        { text: "Podcasts", image: "image1.jpeg" },
-        { text: "Podcasts", image: "image1.jpeg" },
-        { text: "Ambient", image: "image4.jpeg" },
+    var genreArray = [{
+            text: "Podcasts",
+            image: "image1.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Indie",
+            image: "image3.png"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Music",
+            image: "image2.jpeg"
+        },
+        {
+            text: "Podcasts",
+            image: "image1.jpeg"
+        },
+        {
+            text: "Podcasts",
+            image: "image1.jpeg"
+        },
+        {
+            text: "Ambient",
+            image: "image4.jpeg"
+        },
     ];
 
     // Array of colors
@@ -130,4 +142,595 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Code for any other functionality can go here
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Array of image and text combinations
+    var genreArray = [
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },{
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Indie India",
+            image: "image-1.png"
+        },
+        {
+            text: "Punjabi 101",
+            image: "image-2.png"
+        },
+        {
+            text: "Pink Blue",
+            image: "image-3.png"
+        },
+        {
+            text: "Khyaal",
+            image: "image-4.png"
+        },
+        {
+            text: "Chill Station",
+            image: "image-5.png"
+        },
+        {
+            text: "Carnival",
+            image: "image-6.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        {
+            text: "Winters of love",
+            image: "image-77.png"
+        },
+        
+    ];
+
+    // Shuffle the genreArray to randomize the combinations
+    shuffleArray(genreArray);
+
+    // Get all elements with the class "genre-img" and "genre-text"
+    var imgElements = document.querySelectorAll("#card-thumbnail");
+    var textElements = document.querySelectorAll("#card-title");
+
+    // Function to shuffle an array randomly
+    function shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    // Loop through each card and assign the shuffled combinations
+    for (var i = 0; i < imgElements.length; i++) {
+        imgElements[i].src = "/images/" + genreArray[i].image;
+        textElements[i].textContent = genreArray[i].text;
+    }
 });
