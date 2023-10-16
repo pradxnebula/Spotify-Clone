a2cc215d3bbd57b0d3b4c7778405a254e3ca6561
PradhyumShivhare <143643006+pradxnebula@users.noreply.github.com>
2023-10-16 23:58:44 +0530

    Updated Version

--- switch.js
+++ switch.js
@@ -13,3 +13,39 @@
 
 // Initially show the default content (content-1)
 showContent('content-1');
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const homeTab = document.querySelector('.home');
+  const searchTab = document.querySelector('.search');
+  const upperAdditional = document.querySelector('.upper');
+  const lowerAdditional = document.querySelector('.lower');
+
+  homeTab.addEventListener('click', function() {
+    homeTab.classList.add('selected');
+    homeTab.classList.remove('unselected');
+    searchTab.classList.remove('selected');
+    searchTab.classList.add('unselected');
+
+    // Apply additional1 to the upper <p> and additional2 to the lower <p>
+    upperAdditional.classList.add('additional1');
+    upperAdditional.classList.remove('additional2');
+    lowerAdditional.classList.add('additional2');
+    lowerAdditional.classList.remove('additional1');
+  });
+
+  searchTab.addEventListener('click', function() {
+    searchTab.classList.add('selected');
+    searchTab.classList.remove('unselected');
+    homeTab.classList.remove('selected');
+    homeTab.classList.add('unselected');
+
+    // Apply additional2 to the upper <p> and additional1 to the lower <p>
+    upperAdditional.classList.add('additional2');
+    upperAdditional.classList.remove('additional1');
+    lowerAdditional.classList.add('additional1');
+    lowerAdditional.classList.remove('additional2');
+  });
+});
